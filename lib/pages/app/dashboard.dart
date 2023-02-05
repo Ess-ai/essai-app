59b7109136131b290eab34db8497eb02a50159e4
JosephGakah <joegakah@gmail.com>
2023-02-05 23:48:38 +0300

    dashboard sidebar

--- lib/pages/app/dashboard.dart
+++ lib/pages/app/dashboard.dart
@@ -11,6 +11,7 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
+    var _isActive;
     return Scaffold(
         backgroundColor: Colors.transparent,
         body: Row(children: [
@@ -22,13 +23,66 @@ class _DashboardState extends State<Dashboard> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
+                children: [
+                  const Text(
                     'Essai',
                     style: TextStyle(
                         fontSize: 32,
                         fontFamily: 'Blanka',
                         color: Color.fromARGB(255, 2, 21, 80)),
+                  ),
+                  Row(children: const [
+                    Icon(Icons.dashboard),
+                    Text('Dashboard'),
+                  ]),
+                  Container(
+                    color: _isActive,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Profile'),
+                        Row(children: const [
+                          Icon(Icons.dashboard),
+                          Text('Dashboard'),
+                        ]),
+                        Row(children: const [
+                          Icon(Icons.dashboard),
+                          Text('Dashboard'),
+                        ]),
+                        Row(children: const [
+                          Icon(Icons.dashboard),
+                          Text('Dashboard'),
+                        ]),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: _isActive,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Learn'),
+                        Row(children: const [
+                          Icon(Icons.dashboard),
+                          Text('Dashboard'),
+                        ]),
+                        Row(children: const [
+                          Icon(Icons.dashboard),
+                          Text('Dashboard'),
+                        ]),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: _isActive,
+                    child: Row(
+                      children: const [
+                        Icon(Icons.person),
+                        Icon(Icons.explore),
+                      ],
+                    ),
                   ),
                 ],
               ),
