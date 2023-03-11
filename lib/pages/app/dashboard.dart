bfe7ca9e9981d770370a451304e29fbff146d74c
JosephGakah <joegakah@gmail.com>
2023-03-11 14:44:36 +0300

    Popular Tools - Responsive

--- lib/pages/app/dashboard.dart
+++ lib/pages/app/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:essai/pages/app/navigation/footer.dart';
 import 'package:essai/pages/app/navigation/header.dart';
+import 'package:essai/pages/app/widgets/popular_tools.dart';
 import 'package:essai/pages/app/widgets/recent_essays.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,9 +18,8 @@ class DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    var padding = screenWidth * 0.05;
-    print(screenWidth);
+    double width = MediaQuery.of(context).size.width;
+    var padding = width * 0.05;
     return Scaffold(
         body: Column(
       children: [
@@ -180,199 +180,7 @@ class DashboardState extends State<Dashboard> {
                 ),
 
                 //Popular Tools List
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0)),
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.15,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border:
-                                  Border.all(width: 0.5, color: Colors.grey)),
-                          alignment: Alignment.topLeft,
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            alignment: Alignment.topLeft,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Iconsax.scan,
-                                  size: 30,
-                                  color: Colors.black,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Document Scanner',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'No need to Type your essay, Just Scan with your camera.',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black, fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          )),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0)),
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.15,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border:
-                                  Border.all(width: 0.5, color: Colors.grey)),
-                          alignment: Alignment.topLeft,
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            alignment: Alignment.topLeft,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Iconsax.document,
-                                  size: 30,
-                                  color: Colors.black,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'File Marker',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Upload your typed essay from multiple formats e.g docs, img',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black, fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          )),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0)),
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.15,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border:
-                                  Border.all(width: 0.5, color: Colors.grey)),
-                          alignment: Alignment.topLeft,
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            alignment: Alignment.topLeft,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Iconsax.pen_add,
-                                  size: 30,
-                                  color: Colors.black,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Write',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Write or paste your essay here with our writing tool',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black, fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          )),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                          padding: const EdgeInsets.all(0)),
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.15,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              border:
-                                  Border.all(width: 0.5, color: Colors.grey)),
-                          alignment: Alignment.topLeft,
-                          child: Container(
-                            padding: const EdgeInsets.all(8),
-                            alignment: Alignment.topLeft,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Icon(
-                                  Iconsax.d_cube_scan,
-                                  size: 30,
-                                  color: Colors.black,
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Essay Zone',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  'Get Essay Prompts and even generate or get essay samples.',
-                                  textAlign: TextAlign.left,
-                                  style: GoogleFonts.ptSans(
-                                      color: Colors.black, fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          )),
-                    ),
-                  ],
-                ),
+                const PopularTools(),
                 const SizedBox(
                   height: 20,
                 ),
