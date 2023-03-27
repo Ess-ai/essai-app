7ad01bbecfabb93c8b2403ad72b3d8c981f19b38
JosephGakah <joegakah@gmail.com>
2023-03-27 23:30:56 +0300

    display all essays

--- lib/pages/app/essay/essay.dart
+++ lib/pages/app/essay/essay.dart
@@ -9,7 +9,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:lottie/lottie.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+//import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Essay extends StatefulWidget {
   const Essay({Key? key}) : super(key: key);
@@ -20,9 +20,6 @@ class Essay extends StatefulWidget {
 
 class EssayState extends State<Essay> {
   final getEssays = GetEssays();
-  final List<ChartData> chartData = [
-    ChartData('Performance', 78),
-  ];
 
   bool _isMarking = false;
 
@@ -38,11 +35,6 @@ class EssayState extends State<Essay> {
   }
 
   EssayModel essay = EssayModel();
-
-  final String title = 'Plastics Should be Banned';
-  final String title2 = 'Why Plastics Should be Banned';
-  final String _essay =
-      'Plastics have become an integral part of modern society, and their production and usage have increased dramatically over the past few decades. Plastics are versatile, lightweight, durable, and inexpensive, making them ideal for various applications. However, the widespread use of plastics has also led to significant environmental problems, and the negative impacts of plastics are becoming more apparent. In this essay, I will argue that plastics should be banned due to their detrimental effects on the environment and human health.\n\nFirst and foremost, plastics have severe consequences for the environment. Plastics are not biodegradable, meaning that they do not break down naturally and can persist in the environment for hundreds of years. As a result, they accumulate in the oceans, landfills, and other ecosystems, leading to a range of environmental problems. Plastic waste in the oceans poses a significant threat to marine life, as it can be mistaken for food and ingested, leading to injury or death. Plastics also release toxic chemicals as they degrade, contaminating soil and water resources.\n\nMoreover, plastics are a significant source of greenhouse gas emissions. The production of plastics is energy-intensive and involves the extraction and processing of fossil fuels. Additionally, the transportation and disposal of plastics also contribute to greenhouse gas emissions. These emissions contribute to climate change, which has far-reaching environmental and social impacts, including rising sea levels, more frequent natural disasters, and food and water scarcity.\n\nFurthermore, plastics have harmful effects on human health. Plastic products contain chemicals such as phthalates and bisphenol-A, which have been linked to a range of health problems, including reproductive disorders, developmental problems in children, and cancer. These chemicals can leach into food and water, especially when plastics are exposed to heat or other stressors, and can cause significant health risks.\n\nIn conclusion, plastics should be banned due to their negative impact on the environment and human health. Although plastics have numerous benefits, the costs of their production, usage, and disposal far outweigh the benefits. Governments, businesses, and individuals need to take action to reduce the use of plastics and transition to more sustainable alternatives. This can include measures such as introducing plastic taxes, promoting reusable and biodegradable products, and investing in research and innovation to find alternative materials. By taking action to ban plastics, we can protect the environment and safeguard the health of current and future generations.';
 
   Widget essayActionButtons() {
     return Row(children: [
@@ -122,7 +114,7 @@ class EssayState extends State<Essay> {
           padding: const EdgeInsets.only(left: 10),
           child: Column(
             children: [
-              //Score
+              /*Score
               Container(
                 alignment: Alignment.topLeft,
                 padding: const EdgeInsets.all(5),
@@ -159,7 +151,7 @@ class EssayState extends State<Essay> {
                     ]),
               ),
               const SizedBox(height: 20),
-
+              */
               //Marks
               Container(
                   alignment: Alignment.topLeft,
@@ -176,7 +168,7 @@ class EssayState extends State<Essay> {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        '.../20',
+                        essay.isSubmitted == null ? 'Not Submitted' : 'loading',
                         style: GoogleFonts.ptSans(
                             fontSize: 18,
                             color: Colors.lightBlue,
@@ -202,7 +194,7 @@ class EssayState extends State<Essay> {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        '...',
+                        essay.isSubmitted == null ? 'Not Submitted' : 'loading',
                         style: GoogleFonts.ptSans(
                             fontSize: 18,
                             color: Colors.green,
@@ -228,7 +220,7 @@ class EssayState extends State<Essay> {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        '${_essay.split(' ').length}',
+                        '${essay.essayBody!.split(' ').length}',
                         style: GoogleFonts.ptSans(
                             fontSize: 18,
                             color: Colors.red,
@@ -254,7 +246,7 @@ class EssayState extends State<Essay> {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        '${_essay.characters.length}',
+                        '${essay.essayBody!.characters.length}',
                         style: GoogleFonts.ptSans(
                             fontSize: 18,
                             color: Colors.red,
