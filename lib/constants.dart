0cfbe235d5c2a901e046a81c255ee3354326ab76
JosephGakah <joegakah@gmail.com>
2023-05-06 13:41:59 +0300

    Windows Icon

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,14 @@
 class AppConstants {
   final String supabaseUrl = const String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: 'https://jlzlivadcmgelnasocxl.supabase.co',
+    defaultValue: '',
   );
   final String supabaseAnonKey = const String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsemxpdmFkY21nZWxuYXNvY3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4MjIwNjksImV4cCI6MTk5NDM5ODA2OX0.5HJBaTEPKkPATNwSqWROVAhxhMBAdmAq4WyruXigmiY',
+    defaultValue: '',
   );
   final String openaiApiKey = const String.fromEnvironment(
     'OPENAI_API_KEY',
-    defaultValue: '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',
+    defaultValue: '',
   );
 }
